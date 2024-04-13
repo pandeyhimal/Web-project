2f628a075e5f1e9768775481a927c08efd17965b
Himal pandey <141811411+pandeyhimal@users.noreply.github.com>
2024-04-13 13:52:47 +0545

    Add files via upload

---
+++ Web_projects/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add code here to handle form submission, such as sending the form data to a server using AJAX
+});
